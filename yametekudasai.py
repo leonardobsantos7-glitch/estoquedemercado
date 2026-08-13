b191979e0030299554a64d2acbfa47db254827f3
LeoSantosBR <leonardo.b.santos7@aluno.senai.br>
2026-08-12 21:17:08 -0300

    Update yametekudasai.py
    Adição de alteração de produtos em estoque

--- yametekudasai.py
+++ yametekudasai.py
@@ -1,7 +1,7 @@
 estoque = ["arroz"]
 
 while True:
-  acao = input("Deseja adicionar, remover ou sair? ").lower()
+  acao = input("Deseja adicionar, remover, alterar ou sair? ").lower()
 
   if acao == "sair":
     print("Encerrando o programa, produtos atuais em estoque:", estoque)
@@ -22,4 +22,19 @@ while True:
       print("Produto adicionado com sucesso! Produtos no estoque:", estoque)
     else:
       print("O produto já está em estoque!")
+
+  elif acao == "alterar":
+    produto = input("Digite o nome do produto que deseja alterar: ").lower()
+
+    if produto in estoque:
+        novo_produto = input("Digite o novo nome do produto: ").lower()
+
+        if novo_produto not in estoque:
+            indice = estoque.index(produto)
+            estoque[indice] = novo_produto
+            print("Produto alterado com sucesso!", estoque)
+        else:
+            print("Esse produto já existe no estoque!")
+    else:
+        print("O produto não está em estoque!")
 
